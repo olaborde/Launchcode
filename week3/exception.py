93366c8e1938be81624597a20f8dad6c3586f269
olaborde <oml007@live.com>
2018-08-06 19:14:50 -0400

    more exception in python

--- week3/exception.py
+++ week3/exception.py
@@ -1,9 +1,10 @@
 
 
+
 try:
   a = 20
   b = '5'
-  print( a + b)
+  print( a + c)
 
 except NameError:
   print("Oops, something went wrong")  
@@ -11,3 +12,17 @@ else:
   print('lala')  
 finally:
   print('yay')
+
+
+ print("*************************")
+
+
+try:
+  num_1 = input("Enter the first number:")
+  num_2 = input("Enter the second number:")
+
+except ZeroDivisionError:
+  print("PLease enter a number that is not sero innthe second number")  
+
+except TypeError:
+  print('Please enter numbers only')  
