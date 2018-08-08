13ab18ec1148a08915313da7df9f7fc3d24de428
olaborde <oml007@live.com>
2018-08-08 18:39:13 -0400

    more nesting

--- week3/Nesting.py
+++ week3/Nesting.py
@@ -1,4 +1,21 @@
+
+
+# Nested Loops
+
+
 for n in range(3):
-      for x in range(2):
+  for x in range(2):
     print('Hip')
-  print("Hooray")  
+  print("Hooray") 
+
+
+
+group_of_names =[
+  ['joe','lou','blake'],
+    ['max', 'peter', 'charles'],
+    ['Zack','Bibi', 'boo']
+  ]
+
+for names in group_of_names:
+  for name in names:
+    print(name)
