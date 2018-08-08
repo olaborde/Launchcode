c3fe541e128ea9c910f39b5f4bec26b191fe8cd5
olaborde <oml007@live.com>
2018-08-08 18:53:56 -0400

    using len

--- week3/Nesting.py
+++ week3/Nesting.py
@@ -20,3 +20,21 @@ for names in group_of_names:
   for name in names:
     print(name)
 print(group_of_names[1][1])    
+
+
+g_of_names =[
+  ['joe','lou','blake'],
+    ['max', 'peter', 'charles'],
+    ['Zack','Bibi', 'boo']
+  ]
+
+num_of_letters = 0
+
+for group in g_of_names:
+  for name in group:
+    if len( name ) > num_of_letters:
+      num_of_letters = len( name)
+
+
+print('THe largest name in your list is', name, "it has ", num_of_letters)
+
