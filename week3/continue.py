191e96922ba59dced3521b772a23019467d69a21
olaborde <oml007@live.com>
2018-08-08 19:28:42 -0400

    more examples with continue

--- week3/continue.py
+++ week3/continue.py
@@ -2,3 +2,17 @@ for n in range(100):
     if n % 2 == 0:
         continue
 print(n)  
+
+
+
+print("************************Leap year with break and continue*************")
+
+for y in range(1000, 2030):
+    if y % 400 == 0:
+        print(y)
+        break
+    elif y % 100 == 0:
+        continue
+    elif y % 4 == 0:
+        print(y)
+        break    
