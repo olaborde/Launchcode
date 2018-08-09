60b6cab106cec69d93136c52215da02a7bb4ea13
olaborde <oml007@live.com>
2018-08-08 20:54:56 -0400

    Pseudocode

---
+++ week3/Pseudocode.py
@@ -0,0 +1,30 @@
+# #Introduction to Pseudocode
+
+# if you are given a list of lists colors,
+# print the containing list if one of the colors in that list is purple
+
+
+# 1- my original list will be name "Main_List"
+# 2- for each sublist in Main_List, check the color of each of the values
+# 3-if the color in that sublist is purple
+# 4-Print the sublist
+
+
+# GIVEN Main_List
+# FOR each sublist in Main_List
+#     FOR each color in that sublist
+#         IF color is purple
+#             PRINT sublist
+
+
+
+#**************Example # 2**************************************************
+
+#Given a list of user accounts; wiht name, email, addess, phone number and gender, return differetn list of users wgo are males wiht the areac code 954
+
+# 1- get the list_of_users
+# 2- create an empty new_list, waiting for the users
+# 3- go through each user
+# 4- get the gender 
+# 5- if the gender is male
+# 6- check the area code 
