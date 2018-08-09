7408cae8c11dac0212fe02728ecd07a9f8436b41
olaborde <oml007@live.com>
2018-08-08 20:56:36 -0400

    Pseudocode

--- week3/Pseudocode.py
+++ week3/Pseudocode.py
@@ -1,5 +1,7 @@
 # #Introduction to Pseudocode
 
+
+#**************Example # 1 **************************************************
 # if you are given a list of lists colors,
 # print the containing list if one of the colors in that list is purple
 
@@ -18,7 +20,7 @@
 
 
 
-#**************Example # 2**************************************************
+#**************Example # 2 **************************************************
 
 #Given a list of user accounts; wiht name, email, addess, phone number and gender, return differetn list of users wgo are males wiht the areac code 954
 
@@ -28,3 +30,16 @@
 # 4- get the gender 
 # 5- if the gender is male
 # 6- check the area code 
+# 7- if the areacode is 954
+# 8- add the user to a new_list
+
+
+# GIVEN list_of_users
+# CREATE empty new_list
+
+
+# For each user in the list of list_of_users
+#     IF the user's gender is male
+#         IF the user's areacode is 954
+#             Add user to new_list
+# RETURN new_List            
