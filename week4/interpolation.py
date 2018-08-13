1fc1e027918cdfd2b129a032d1f713dc7787b0bb
olaborde <oml007@live.com>
2018-08-13 19:42:12 -0400

    string interpolation

---
+++ week4/interpolation.py
@@ -0,0 +1,22 @@
+# The print function
+
+# string Interpolation
+
+
+print( 'helo,{}, my name is {}'.format('Al', 'bob'))
+
+print()
+
+a = 'bob'
+b = 'jow'
+
+print(f'Hello,{a}, my name is {b}')
+
+# percentage format
+
+
+name = 'Braun'
+
+money = 5000
+
+print(' %s has %f in the bank' % (name, money))
