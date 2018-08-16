54dd49505fac2aac4eaa4a4db063c6294259bfc8
olaborde <oml007@live.com>
2018-08-16 13:07:43 -0400

    more operation on a list

--- week4/methods_list.py
+++ week4/methods_list.py
@@ -32,3 +32,76 @@ myWord = ''.join(myWordList)
 
 print(myWord)
     
+
+
+# List 
+myList = ['a','b','c', 'd','e', 'f','g', 'h','i']
+
+
+
+print(myList[0:6])
+
+print(myList[-2:2])
+
+
+print(myList[3:7:2])
+
+
+print(myList[-2:1:-1])
+
+
+print(myList[3:7:-1])
+
+# Objects and references
+# Python and Memory
+#immutable 
+name_1 = 'Banana'
+word_1 = 'Banana'
+
+print(name_1 == word_1)
+print(name_1 is word_1)
+
+# too different list (mulatab;e)
+list_1 = ['banana', 12, 'milk']
+list_2 = ['banana', 12, 'milk']
+
+print(list_1 == list_2)
+print(list_1 is list_2)
+# is TRUE is they are pointing to the same object
+
+# reassigning 
+
+#Accumulation
+
+a = 'Hello'
+
+a += 'Mark'
+
+
+print(a)
+
+a = [10,20,30,40,50,50,70,80,90]
+
+b = a
+
+a[2] =1920
+
+print ( b )
+
+
+print ( a )
+
+#cloning
+
+c = a[:]
+
+print(c)
+
+# lists are mutable strings are immutable
+
+
+my_list = [10, 20, 30, 40, 50]
+
+[item * 2 for item in my_list]
+
+
