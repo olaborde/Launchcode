bbf4bd7b2d6cd5f6b467e7bdd82d72fb4e38de4d
olaborde <oml007@live.com>
2018-08-27 18:52:18 -0400

    mod class

--- week6/class.py
+++ week6/class.py
@@ -1,12 +1,29 @@
+
+
+#   def start(self)   
+#---------------------------------- Properties (parameters)
+#                     |    |      |
+#                     v    v      v
 class Person:
   def __init__(self, name, gndr, occp):
     # variable ---
     #            |
+    #            V 
     self.name = name  
     self.gender = gndr
     self.occupation = occp
 
+  def greet(self):
+    return "Hello, my name is " + self.name  
+
+  def greet_person(self, otherName):
+    return "Hello, " + otherName + " my name is "  + self.name  
+
 mark = Person( "Mark", "Male", "Surgeon" ) 
+suzie = Person( "Suzie", "Female", "Lawyer" ) 
 
 print(mark.gender)
-print(mark.name)   
+print(mark.name)  # properties   
+
+print(mark.greet())  # methods
+print(suzie.greet_person(mark.name))  # methods 
