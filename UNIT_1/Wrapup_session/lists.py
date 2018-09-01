d03d7ca2dec85d5c011da61dc870e08be217a984
olaborde <oml007@live.com>
2018-09-01 13:38:08 -0400

    manual list

--- UNIT_1/Wrapup_session/lists.py
+++ UNIT_1/Wrapup_session/lists.py
@@ -1,4 +1,18 @@
 
 
+# def list
 
-myFruitsLists = []
+
+myFruitsLists = [ 'Banana', 'Mangoes', 'Strawberries' ] 
+
+
+counter = 1
+
+for items in myFruitsLists:
+    print ( counter, items)
+    counter += 1
+
+# add more items to the lists
+
+myFruitsLists.append( ' Coconuts ')  
+myFruitsLists.append( ' Kiwi ')     
