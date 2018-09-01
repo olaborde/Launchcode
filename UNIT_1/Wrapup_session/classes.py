bc827ce15035abc9406e47861b914308a8beb33e
olaborde <oml007@live.com>
2018-09-01 15:31:31 -0400

    created person class

---
+++ UNIT_1/Wrapup_session/classes.py
@@ -0,0 +1,22 @@
+
+
+# classes are blueprint of what an object should
+
+
+class Person:
+    
+    def __init__(self, ):
+        # required - unique
+        self.name
+        self.gender
+        self.age
+        self.height
+        self.weight
+        self.race
+        self.eyecolor
+        self.haircolor
+
+        # required
+        self.alive = True
+        self.legs = 2
+        self.arms = 2
