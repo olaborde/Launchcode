74af7d3113393be92f6904584a38ee2489fafc48
olaborde <oml007@live.com>
2018-09-01 13:11:12 -0400

    generateURL slug

--- UNIT_1/Wrapup_session/functions.py
+++ UNIT_1/Wrapup_session/functions.py
@@ -6,19 +6,28 @@
 # function that removes - and replace with a .
 # .replace( "-", ".")
 
-def removeSpaces():
+def generateUrlSlug( titleOfBlog ):
+    print( titleOfBlog)
     # remove spaces at the beginning and end of my input
+    strippedTitle = titleOfBlog.strip()
     # replace all spaces inside with dashes
+    dashedTitle = strippedTitle.replace(" ", "-")
+    # make everything character lower case
+    lowerCaseTitle = dashedTitle.lower()
     # return the result
+    
+    return lowerCaseTitle
 
 
 
 def main():
     pass
-    theTitle = input(" What is the tite of your blog")
-    print( "Here is the url of the blog:" )
-    print( removeSpaces() )
+    theTitle = input(" What is the tite of your blog: ")
 
-if __name__ = "__main__"
+    print( "Here is the url of the blog:" )
+    urlReady = generateUrlSlug( theTitle )
+    print( urlReady)
+
+if __name__ == "__main__":
 
     main()
