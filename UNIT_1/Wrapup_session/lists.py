6496e1f4951142a786dcff3e6b4dcad398a5ebf9
olaborde <oml007@live.com>
2018-09-01 13:42:24 -0400

    use a function to modify the list

--- UNIT_1/Wrapup_session/lists.py
+++ UNIT_1/Wrapup_session/lists.py
@@ -1,18 +1,23 @@
 
 
-# def list
+def list_my_fruits( myItems ):
+
+    counter = 1
+
+    for items in myFruitsLists:
+        print ( counter, items)
+        counter += 1
 
 
 myFruitsLists = [ 'Banana', 'Mangoes', 'Strawberries' ] 
 
 
-counter = 1
+list_my_fruits( myFruitsLists )
 
-for items in myFruitsLists:
-    print ( counter, items)
-    counter += 1
+
 
 # add more items to the lists
 
 myFruitsLists.append( ' Coconuts ')  
 myFruitsLists.append( ' Kiwi ')     
+list_my_fruits( myFruitsLists )
