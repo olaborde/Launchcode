7ed6d3b63447f07f31473f603ea992f221d815cf
olaborde <oml007@live.com>
2018-09-01 12:46:01 -0400

    generateURL functions

--- UNIT_1/Wrapup_session/functions.py
+++ UNIT_1/Wrapup_session/functions.py
@@ -1,11 +1,23 @@
+# with parameters -> accepts inputs
+#without parameters -> does not accept inputs
+#fruitful function  -> returns data
+# non-fruitful  --> does not return data
 
+# function that removes - and replace with a .
+# .replace( "-", ".")
 
-
+def removeSpaces():
+    # remove spaces at the beginning and end of my input
+    # replace all spaces inside with dashes
+    # return the result
 
 
 
 def main():
     pass
+    theTitle = input(" What is the tite of your blog")
+    print( "Here is the url of the blog:" )
+    print( removeSpaces() )
 
 if __name__ = "__main__"
 
