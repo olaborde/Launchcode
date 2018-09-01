8196162423219bee22754bc3f3c495e955a7b640
olaborde <oml007@live.com>
2018-09-01 15:40:29 -0400

    added methods to person classs

--- UNIT_1/Wrapup_session/classes.py
+++ UNIT_1/Wrapup_session/classes.py
@@ -5,7 +5,7 @@
 
 class Person:
     
-    def __init__(self, name, gender, age, height, weight, race, eyecolor, haircolor):
+    def __init__(self, name, gender, age, height, weight, race, eyecolor, haircolor = "black"):
         # required - unique
         self.name = name
         self.gender = gender
@@ -15,8 +15,29 @@ class Person:
         self.race = race
         self.eyecolor = eyecolor
         self.haircolor = haircolor
+        self.hungry = True
+        self.tired = True
 
         # required
         self.alive = True
         self.legs = 2
         self.arms = 2
+
+
+    def walk(self):
+        return self.name + "walks forward"   
+
+
+    def say(self, sentence):
+        return self.name + "Says" + sentence 
+
+
+    def eat(self):
+        return self.hungry = False
+
+    def sleep(self):
+        self.tired = False
+        self.hungry = True
+
+    def run(self):
+        self.tired = True       
