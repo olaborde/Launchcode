9b50d0536916bd2f964c822d9d5601f2e144d287
olaborde <oml007@live.com>
2018-09-02 02:28:53 -0400

    added chapter 13 assignment

---
+++ UNIT_1/week6/chapter13_assignment.py
@@ -0,0 +1,38 @@
+class Chatbot:
+    """ An object that can engage in rudimentary conversation with a human. """
+
+    def __init__(self, name):
+        self.name = name
+
+    def greeting(self):
+        """ Returns the Chatbot's way of introducing itself. """
+        return "Hello, my name is " + self.name
+
+    def response(self, prompt_from_human):
+        """ Returns the Chatbot's response to something the human said. """
+        return "It is very interesting that you say: '" + prompt_from_human + "'"
+
+
+# TODO define a class called BoredChatbot
+class BoredChatbot(Chatbot):
+    
+    
+    
+    
+    def tooLong(self, the_message):
+        #human_message = input(sally.greeting())
+
+     
+
+        if len( the_message) > 20:
+            
+            
+            return  'zzz... Oh excuse me, I dozed off reading your essay.'
+        else:
+            return sally.response(human_message)
+
+
+        
+        
+
+
