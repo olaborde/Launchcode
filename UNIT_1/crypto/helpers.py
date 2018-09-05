37f85dd03d172b67916755053df784642da0e350
olaborde <oml007@live.com>
2018-09-05 11:47:10 -0400

    finish helpers function

--- UNIT_1/crypto/helpers.py
+++ UNIT_1/crypto/helpers.py
@@ -7,3 +7,20 @@ def alphabet_position(letter):
   for position, i in enumerate(a_z):
     if i == new_letter:
       return position
+
+
+# rotate_character
+def rotate_character(char, rot):
+  #list of string
+  a_z = list(string.ascii_lowercase)
+  # Grab the position of the character to add to rot
+  if str(char).isalpha():
+    new_char = alphabet_position(char)
+    the_char =( new_char + rot ) % 26
+    if char.isupper():
+      return (a_z[the_char].upper())
+    else:
+      return (a_z[the_char])  
+  else:
+    
+    return char   
