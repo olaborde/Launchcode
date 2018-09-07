e2538b85d8cdaf403297c2a895adc8ac13a8800e
olaborde <oml007@live.com>
2018-09-07 02:32:36 -0400

    create grade_converter function

--- UNIT_1/week2/functions.py
+++ UNIT_1/week2/functions.py
@@ -105,3 +105,15 @@ def report():
 print("End of report")
 
 report()
+
+def grade_converter(gpa):
+      if gpa >= 4.0:
+    return "A"
+  elif gpa >= 3.0:
+    return "B"
+  elif gpa >= 2.0:
+    return "C"
+  elif gpa >= 1.0:
+    return "D"
+  else:
+    return "F"
